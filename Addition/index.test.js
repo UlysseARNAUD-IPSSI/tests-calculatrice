f522520272ccf7b93a8fc6445776e1c9d1ffca03
Ulysse ARNAUD <mexanga@gmail.com>
2020-07-15 15:55:30 +0200

    MAJ Mineur sur les tests

--- Addition/index.test.js
+++ Addition/index.test.js
@@ -11,3 +11,7 @@ test('Additionne -1 et 2 pour arriver à 1', () => {
 test('Additionne 1 et -2 pour arriver à -1', () => {
     expect(Addition(1, -2)).toBe(-1);
 });
+
+test('Additionne -1 et -2 pour arriver à -1', () => {
+    expect(Addition(-1, -2)).toBe(-3);
+});
