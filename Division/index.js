bff791e317a82bbe556788199fe534a4b92d3dff
Ulysse ARNAUD <mexanga@gmail.com>
2020-07-15 15:06:17 +0200

    MAJ sur les tests de Division

--- Division/index.js
+++ Division/index.js
@@ -1,10 +1,18 @@
-module.exports = Division;
+const DividedByZeroError = require('../Errors/DividedByZeroError');
 
 /**
  * @param a number
  * @param b number
  * @return {number}
+ * @throws DividedByZeroError
  */
 function Division (a, b) {
+
+    if (0 === b) {
+        throw DividedByZeroError.singleton();
+    }
+
     return a / b;
 }
+
+module.exports = Division;
--- Division/index.test.js
+++ Division/index.test.js
@@ -1,5 +1,22 @@
-const Division = require('./index');
+const Division = require('.');
+const DividedByZeroError = require('../Errors/DividedByZeroError');
 
 test('Divise 25 par 5 pour obtenir 5', () => {
     expect(Division(25, 5)).toBe(5);
+});
+
+test('Divise -25 par 5 pour obtenir -5', () => {
+    expect(Division(-25, 5)).toBe(-5);
+});
+
+test('Divise 25 par -5 pour obtenir -5', () => {
+    expect(Division(25, -5)).toBe(-5);
+});
+
+test('Divise -25 par -5 pour obtenir 5', () => {
+    expect(Division(-25, -5)).toBe(5);
+});
+
+test('Divise un nombre par zero afin d\'obtenir une erreur.', () => {
+    expect(Division(Math.floor(Math.random() * 100) + 1, 0)).toThrow(DividedByZeroError);
 });
