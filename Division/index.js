87df004fcc0f4314f49d964cf21b428c2b678611
Ulysse ARNAUD <mexanga@gmail.com>
2020-07-15 12:53:30 +0200

    Ajout de la division

---
+++ Division/index.js
@@ -0,0 +1,10 @@
+module.exports = Division;
+
+/**
+ * @param a number
+ * @param b number
+ * @return {number}
+ */
+function Division (a, b) {
+    return a / b;
+}
---
+++ Division/index.test.js
@@ -0,0 +1,5 @@
+const Division = require('./index');
+
+test('Divise 25 par 5 pour obtenir 5', () => {
+    expect(Division(25, 5)).toBe(5);
+});
