ff8cf3ffce43f987aec1be41808fe78e432d77d9
Ulysse ARNAUD <mexanga@gmail.com>
2020-07-15 15:23:47 +0200

    Ajout d'un cas dans les tests de Division

--- Division/index.test.js
+++ Division/index.test.js
@@ -17,6 +17,10 @@ test('Divise -25 par -5 pour obtenir 5', () => {
     expect(Division(-25, -5)).toBe(5);
 });
 
-test('Divise un nombre par zero afin d\'obtenir une erreur.', () => {
+test('Divise un nombre positif par zero afin d\'obtenir une erreur.', () => {
     expect(Division(Math.floor(Math.random() * 100) + 1, 0)).toThrow(DividedByZeroError);
+});
+
+test('Divise un nombre négatif par zero afin d\'obtenir une erreur.', () => {
+    expect(Division((Math.floor(Math.random() * 100) + 1) * -1, 0)).toThrow(DividedByZeroError);
 });
